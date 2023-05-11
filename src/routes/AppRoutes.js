c79c635f57407bf51bfbe5bc73e20cfc9c56b430
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2023-05-11 21:02:01 +0530

    implement tour trip frontend

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -8,35 +8,78 @@ import AdminLayout from "../layout/admin/AdminLayout";
 import Header from "../components/admin/header/Header";
 import ClientHeader from "../components/client/Header/Header";
 import Footer from "../components/client/Footer/Footer";
+import TourTripAdmin from "../pages/admin/tourTripManagement/TourTripAdmin";
+import TourTripIndex from "../pages/client/tourTripManagement/index";
+import BookYourtrip from "../pages/client/tourTripManagement/bookYourTrip";
+import BeforeYouGo from "../pages/client/tourTripManagement/beforeYouGo";
+import AboutSriLanka from "../pages/client/tourTripManagement/aboutSriLanka";
+import TourTripDetailsPage from "../pages/client/tourTripManagement/touTripDetailsPage";
 
 const AppRoutes = () => {
 	return (
 		<div>
-        <Router>
-            <Switch>
-                <Route path='/client/:path?' exact>
-                            <ClientHeader/>
-                                <Switch>
-                                
-                                
-                                </Switch>
-                                <Footer/>
-                </Route>
+			<Router>
+				<Switch>
+					<Route path="/client/:path?" exact>
+						<ClientHeader />
+						<Switch>
+							<Route
+								path="/client/tour-trip-details"
+								render={(props) => <TourTripIndex />}
+								exact
+							/>
+							;
+							<Route
+								path="/client/book-your-trip"
+								render={(props) => <BookYourtrip />}
+								exact
+							/>
+							;
+							<Route
+								path="/client/before-you-go"
+								render={(props) => <BeforeYouGo />}
+								exact
+							/>
+							;
+							<Route
+								path="/client/about-sri-lanka"
+								render={(props) => <AboutSriLanka />}
+								exact
+							/>
+							;
+							<Route
+								path="/client/tour-trip"
+								render={(props) => <TourTripDetailsPage />}
+								exact
+							/>
+							;
+						</Switch>
+						<Footer />
+					</Route>
 
-                <Route path='/admin/:path?' exact>
-                <AdminLayout class="wrapper">
-                        <Header/>
-                        <Sidebar/>
-                        <Switch>
-                            <Route path="/admin/productadmin" render={(props) => <ProductAdmin/>} exact/>;
-                        </Switch>
-                    </AdminLayout>
-                </Route>
-
-
-            </Switch>
-        </Router>
-    </div>
+					<Route path="/admin/:path?" exact>
+						<AdminLayout class="wrapper">
+							<Header />
+							<Sidebar />
+							<Switch>
+								<Route
+									path="/admin/productadmin"
+									render={(props) => <ProductAdmin />}
+									exact
+								/>
+								;
+								<Route
+									path="/admin/tourtripadmin"
+									render={(props) => <TourTripAdmin />}
+									exact
+								/>
+								;
+							</Switch>
+						</AdminLayout>
+					</Route>
+				</Switch>
+			</Router>
+		</div>
 	);
 };
 export default AppRoutes;
